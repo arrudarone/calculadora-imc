c451bcbd2edc80f9c48182baeca532d693741197
Rone Arruda <ronearruda04@gmail.com>
2023-04-05 22:23:37 -0300

    validação java

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,4 +1,18 @@
 function calcular(){
     
-    
+    let botao = document.getElementsByClassName('botao')
+    let peso = document.getElementById('ipeso')
+    let altura = document.getElementById('ialtura')
+    let resultado = document.getElementById('res')
+
+    if(peso.value.length == '0' || altura.value.length == '0'){
+
+        window.alert('informe os dados corretamente!')
+         
+        resultado.innerHTML += 'informe os campos corretamente'
+
+    }else if{
+        
+    }
+
 }
