74db31d8fd420321b7e65b7cfc654864fbfe57ab
Rone Arruda <ronearruda04@gmail.com>
2023-04-06 23:02:07 -0300

    demais faixas de imc

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -10,18 +10,37 @@ function calcular(){
 
         window.alert('informe os dados corretamente!')
          
-        resultado.innerHTML += 'informe os campos corretamente'
+        resultado.innerHTML += ' informe os campos corretamente'
 
     }else{
 
         let ms = Number(peso.value)
         let alt = Number(altura.value)
         let imc = ms / (alt * 2)
-
-        resultado.innerHTML += ` ${imc.toFixed(1)} , parabéns`
-
         /*window.alert(`seu imc é: ${imc.toFixed(1)}`)*/
 
+        if(imc <= 18.5){
+            resultado.innerHTML += ` ${imc.toFixed(1)} , Você esta abaixo do peso`
+        }
+        else if( imc <= 24.9){
+
+            resultado.innerHTML += ` ${imc.toFixed(1)} , Você esta com o peso normal`
+        }else if( imc <= 29.9){
+
+            resultado.innerHTML += ` ${imc.toFixed(1)} , Você esta com sobrepeso`
+
+        }else if(imc <= 34.9){
+
+            resultado.innerHTML += ` ${imc.toFixed(1)} , Você esta com Obesidade grau 1`
+
+        }else if( imc <= 39.9){
+
+            resultado.innerHTML += ` ${imc.toFixed(1)} , Você esta com Obesidade grau 2`
+
+        }else if( imc > 40){
+
+            resultado.innerHTML += ` ${imc.toFixed(1)} , Você esta com Obesidade grau 3`
+        }
         
     }
 
